3a53c3cd9c00815634c5af856b20e8276080da1b
Abdelrahman-Talaat <abdelrahmantalaat974@gmail.com>
2023-01-03 00:24:36 +0200

    update hello test

--- src/test/java/Pages/HelloTest.java
+++ src/test/java/Pages/HelloTest.java
@@ -1,16 +1,26 @@
 package Pages;
 
 import BaseTest.Base;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.*;
 
 public class HelloTest  {
    public WebDriver driver;
     private HelloPage helloPage;
     @Test
     public void helloTest(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        assertTrue(driver.getTitle().contains("Google"));
         helloPage=new HelloPage(driver);
-     helloPage.print();
+        helloPage.print();
+        driver.quit();
     }
 
 }
