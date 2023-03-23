0406bc7ab4a6d11f013456020e787bdf656ee745
Abdelrahman-Talaat <abdelrahmantalaat974@gmail.com>
2023-03-24 01:44:39 +0200

    Add chrome options in pom.xml file

--- src/test/java/Pages/HelloTest.java
+++ src/test/java/Pages/HelloTest.java
@@ -3,6 +3,7 @@ package Pages;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import static org.testng.AssertJUnit.*;
@@ -13,7 +14,9 @@ public class HelloTest  {
     @Test
     public void helloTest(){
         WebDriverManager.chromedriver().setup();
-        driver=new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*","ignore-certificate-errors");
+        driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.google.com/");
         driver.manage().window().maximize();
         assertTrue(driver.getTitle().contains("Google"));
