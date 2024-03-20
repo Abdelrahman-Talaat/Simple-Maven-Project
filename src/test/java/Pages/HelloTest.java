9a6ef775fcadd770065ec44327cd8a2d61abc654
Abdelrahman Talaat <abdelrahmantalaat974@gmail.com>
2024-03-20 02:33:13 +0200

    Update HelloTest.java

--- src/test/java/Pages/HelloTest.java
+++ src/test/java/Pages/HelloTest.java
@@ -1,14 +1,25 @@
 package Pages;
 
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
-public class HelloPage {
-     WebDriver driver;
-    public HelloPage(WebDriver driver) {
-        this.driver=driver;
+
+public class HelloTest  {
+    WebDriver driver;
+     HelloPage helloPage;
+    @Test
+    public void helloTest(){
+        driver=new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+       Assert.assertTrue(driver.getTitle().contains("Google"));
+        helloPage=new HelloPage(driver);
+        helloPage.print();
+        driver.quit();
     }
 
-    public void print(){
-        System.out.println("Hello World");
-    }
 }
