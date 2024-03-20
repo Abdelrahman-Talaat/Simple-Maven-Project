8d6c91db2073fcc4dfcaec2106d4480c446fcbb2
Abdelrahman Talaat <abdelrahmantalaat974@gmail.com>
2024-03-20 02:32:24 +0200

    Update HelloTest.java

--- src/test/java/Pages/HelloTest.java
+++ src/test/java/Pages/HelloTest.java
@@ -1,28 +1,14 @@
 package Pages;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.Test;
 
-import static org.testng.AssertJUnit.*;
-
-public class HelloTest  {
-   public WebDriver driver;
-    private HelloPage helloPage;
-    @Test
-    public void helloTest(){
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*","ignore-certificate-errors");
-        driver = new ChromeDriver(chromeOptions);
-        driver.get("https://www.google.com/");
-        driver.manage().window().maximize();
-        assertTrue(driver.getTitle().contains("Gogle"));
-        helloPage=new HelloPage(driver);
-        helloPage.print();
-        driver.quit();
+public class HelloPage {
+     WebDriver driver;
+    public HelloPage(WebDriver driver) {
+        this.driver=driver;
     }
 
+    public void print(){
+        System.out.println("Hello World");
+    }
 }
